5ff84c9604b07f8b68d7d15f864e462697e10a75
ashleyisles <aisles@smu.edu>
2015-03-22 16:48:00 -0500

    Update dbCreate.sql

--- database/dbCreate.sql
+++ database/dbCreate.sql
@@ -4,15 +4,15 @@ USE 'VirtualPantryDB';
 CREATE TABLE IF NOT EXISTS User (uid int NOT NULL AUTO_INCREMENT, fname varchar(30), lname varchar(30), email varchar(45), 
 					password varchar(50), did int, srid int, PRIMARY KEY(uid));
 
-CREATE TABLE IF NOT EXISTS PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid int, barcode int, 
+CREATE TABLE IF NOT EXISTS PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid varchar(20), barcode int, 
 						 price decimal(5,2), pname varchar(50), PRIMARY KEY(pantryid), FOREIGN KEY(u_id)
 						 REFERENCES User(uid) ON DELETE CASCADE);
 
-CREATE TABLE IF NOT EXISTS Ingredient (pid int, fat int, chol int, sodium int, carb int, protien int,
+CREATE TABLE IF NOT EXISTS Ingredient (pid varchar(20), fat int, chol int, sodium int, carb int, protien int,
 						 servsize int, price decimal(5,2), barcode int, cal int, exp date, 
-						 pname varchar(50), PRIMARY KEY(pid));
+						 pname varchar(50), PRIMARY KEY(barcode));
 
-CREATE TABLE IF NOT EXISTS ExpiredList (pid int, barcode int, price decimal(5,2), PRIMARY KEY(pid));
+CREATE TABLE IF NOT EXISTS ExpiredList (pid varchar(20), barcode int, price decimal(5,2), PRIMARY KEY(barcode));
 
 
 
