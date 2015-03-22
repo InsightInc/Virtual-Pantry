b41a03ea4833ec3853c0d7198d7cdf213b8905d4
ashleyisles <aisles@smu.edu>
2015-03-22 16:41:39 -0500

    Update populateDBtesting.sql

--- database/populateDBtesting.sql
+++ database/populateDBtesting.sql
@@ -19,18 +19,6 @@ INSERT into Pantrylist values(1, 1, 40000000002, 1234321, 5.99, "Bacon Strips"),
 (4, 4, 40000000005, 28163834, 2.79, "Red Apple"),
 (5, 5, 40000000003, 66996699, 3.27, "Pomegranate");
 
--- CREATE TABLE Ingredient (pid int, fat int, chol int, sodium int, carb int, protien int,
--- 						 servsize int, price decimal(5,2), barcode int, cal int, exp date, 
--- 						 pname varchar(50), PRIMARY KEY(pid));
-
--- CREATE TABLE ExpiredList (pid int, barcode int, price decimal(5,2), PRIMARY KEY(pid));
-
--- CREATE TABLE SavedRecipes (srid int, rid int, rname varchar(50), ptime time, ringid int,
--- 						   rprice decimal(5,2), PRIMARY KEY (srid, rid));
-
--- CREATE TABLE RecipeIngredient (ringid int, rid int, pid int, ingamt int, PRIMARY KEY(ringid));
-
--- CREATE TABLE DietaryRestrictions(did int, restrict varchar(50));
 
 
 
