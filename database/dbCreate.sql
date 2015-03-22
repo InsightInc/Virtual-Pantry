1d87185ea6cd95ffd733c00680f5a0e893ff0b14
vagrant <vagrant@scotchbox>
2015-03-22 23:57:38 +0000

    dbcreate script drops table is they exsist

--- database/dbCreate.sql
+++ database/dbCreate.sql
@@ -5,30 +5,19 @@ DROP TABLE IF EXISTS User;
 CREATE TABLE IF NOT EXISTS User (uid int NOT NULL AUTO_INCREMENT, fname varchar(30), lname varchar(30), email varchar(45), 
 					password varchar(50), did int, srid int, PRIMARY KEY(uid));
 
-<<<<<<< HEAD
 DROP TABLE IF EXISTS PantryList;
-CREATE TABLE IF NOT EXISTS PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid int, barcode int, 
-						 price decimal(5,2), pname varchar(50), PRIMARY KEY(pantryid), FOREIGN KEY(u_id)
-						 REFERENCES User(uid) ON DELETE CASCADE);
-
-DROP TABLE IF EXISTS Ingredient;
-CREATE TABLE IF NOT EXISTS Ingredient (pid int, fat int, chol int, sodium int, carb int, protien int,
-						 servsize int, price decimal(5,2), barcode int, cal int, exp date, 
-						 pname varchar(50), PRIMARY KEY(pid));
-
-DROP TABLE IF EXISTS ExpiredList;
-CREATE TABLE IF NOT EXISTS ExpiredList (pid int, barcode int, price decimal(5,2), PRIMARY KEY(pid));
-=======
 CREATE TABLE IF NOT EXISTS PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid varchar(20), barcode varchar(15), 
 						 price decimal(5,2), pname varchar(50), PRIMARY KEY(pantryid), FOREIGN KEY(u_id)
 						 REFERENCES User(uid) ON DELETE CASCADE);
 
+DROP TABLE IF EXISTS Ingredient;
 CREATE TABLE IF NOT EXISTS Ingredient (pid varchar(20), fat int, chol int, sodium int, carb int, protien int,
 						 servsize int, price decimal(5,2), barcode varchar(15), cal int, exp date, 
 						 pname varchar(50), PRIMARY KEY(barcode));
 
+DROP TABLE IF EXISTS ExpiredList;
 CREATE TABLE IF NOT EXISTS ExpiredList (pid varchar(20), barcode varchar(15), price decimal(5,2), PRIMARY KEY(barcode));
->>>>>>> 67d5abb8cab77cd592df7ebf665d56241a6d0a1e
+
 
 DROP TABLE IF EXISTS SavedRecipes;
 CREATE TABLE IF NOT EXISTS SavedRecipes (srid int, rid int, rname varchar(50), ptime time, ringid int,
