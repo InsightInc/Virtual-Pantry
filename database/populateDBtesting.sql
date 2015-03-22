14005ee3447f7feb377727c3e04f98db5a471f4a
ashleyisles <aisles@smu.edu>
2015-03-22 16:52:52 -0500

    Update populateDBtesting.sql

--- database/populateDBtesting.sql
+++ database/populateDBtesting.sql
@@ -9,15 +9,14 @@ INSERT into User values(1, "Ashley", "Isles", "ashley@smu.edu", "test1", 1, 1),
 (4, "Alex", "Russell", "alex@smu.edu", "test1", 4, 4),
 (5, "Conner", "Knuston", "conner@smu.edu", "test1", 5, 5);
 
--- CREATE TABLE PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid int, barcode int, 
+-- CREATE TABLE PantryList (pantryid int NOT NULL AUTO_INCREMENT, u_id int NOT NULL, pid varchar(20), barcode int, 
 -- 						 price decimal(5,2), pname varchar(50), PRIMARY KEY(pantryid), FOREIGN KEY(u_id)
 -- 						 REFERENCES User(uid) ON DELETE CASCADE);
 
-INSERT into Pantrylist values(1, 1, 40000000002, 1234321, 5.99, "Bacon Strips"),
-(2, 2, 40000000002, 12343210, 5.99, "Bacon Strips"),
-(3, 3, 40000000007, 24680864, 25.99, "1 lb Angus Steak"),
-(4, 4, 40000000005, 28163834, 2.79, "Red Apple"),
-(5, 5, 40000000003, 66996699, 3.27, "Pomegranate");
+INSERT into Pantrylist(uid, pid, barcode, pname) values(1, "Bacon", 035826034724, "Bacon"),
+(1, "Oreo", 044000007492, "Oreo"),
+(1,"Apple", 033383027814,"Apple"),
+(1, "Pomegranate", 073296153637, "Pomegranate");
 
 
 
