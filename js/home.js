457f60e9a3c4e5a759da510afe21e78e1b93ad66
Alex Russell <amrussell@smu.edu>
2015-03-23 00:09:01 +0000

    started home.html, added js and datatables files

---
+++ js/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#allRecipes").DataTable({
+        "scrollY":          "200px",
+        "scrollCollapse":   true,
+        "paging":           false
+    });
+});
