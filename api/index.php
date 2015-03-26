12bca5f36d918a6843ed97f26ce4ace0cef3976f
vagrant <vagrant@scotchbox>
2015-03-26 19:41:53 +0000

    Apparently you have to have a message

--- api/index.php
+++ api/index.php
@@ -1,19 +1,23 @@
 <?php
 require 'vendor/autoload.php';
+session_cache_limiter(false);
+session_start();
 $app = new \Slim\Slim();
-
  $database = new mysqli("localhost", "root", "root", "VirtualPantryDB");
  if ($database->connect_errno)
      die("Connection failed: " . $database->connect_error);
-
 #get product by searching by name
 $app->get('/addProduct', function() {
-
 	global $database;
-
 	$name = $_GET['name'];
-	$id = $_GET['uid'];
-
+	if( isset( $_SESSION['uid'] ) )
+   	{
+		 $id =  $_SESSION['uid'];
+   	}
+   	else
+   	{
+      		$id = 1;
+   	}
 	#Connect to foodessentials api ------------------------------------------------
 	#Get session id
 	$sjson = file_get_contents('http://api.foodessentials.com/createsession?uid=ert&devid=ert&appid=ert&f=json&v=2.00&api_key=x4c59ktead886t2urzcdju54');
@@ -40,9 +44,7 @@ $app->get('/addProduct', function() {
     $productList = json_decode($pjson);
     $upc = $productList->productsArray[0]->upc;
     $product = file_get_contents('http://api.foodessentials.com/productscore?u='.$upc.'&sid='.$sid.'&f=json&api_key=x4c59ktead886t2urzcdju54');
-
     $product = json_decode($product);
-
     $jsonProduct = array();
     $jsonProduct['pid'] = $name;
     $jsonProduct['fat'] = $product->product->nutrients[5]->nutrient_value;
@@ -53,61 +55,53 @@ $app->get('/addProduct', function() {
 	$jsonProduct['barcode'] = $upc;
 	$jsonProduct['cal'] = $product->product->nutrients[0]->nutrient_value;
 	$jsonProduct['name'] = $product->product->product_name;
-
-
 	$pname = $jsonProduct['name'];
-
 	$response = $database->query("INSERT INTO PantryList (uid, pid, barcode, pname) VALUES ($id, '$name', $upc, '$pname')");
-
-
     echo $response;
 });
-
 $app->get('/removeProduct', function() 
 {
 	global $database;
 	$name = $_GET['name'];
-	$id = $_GET['uid'];
-
-
+	if( isset( $_SESSION['uid'] ) )
+   	{
+		 $id =  $_SESSION['uid'];
+   	}
+   	else
+   	{
+      		$id = 1;
+   	}
 	$response = $database->query("DELETE FROM PantryList WHERE pid = '$name' AND uid = $id.");
-
 	echo $response;
-
-
 });
-
-
 $app->get('/getPantryList', function() 
 {
 	global $database;
-	$id = $_GET['uid'];
-
-
+	if( isset( $_SESSION['uid'] ) )
+   	{
+		 $id =  $_SESSION['uid'];
+   	}
+   	else
+   	{
+      		$id = 1;
+   	}
 	$response = $database->query("SELECT pname FROM PantryList WHERE uid = $id");
 	$response = $response->fetch_all();
-
 	$response = json_encode($response);
 	echo $response;
-
-
 });
-
-
-$app->get('/getRecipes', function($query)
+$app->get('/getRecipes', function()
 // $app->get('/', function()
 {
 	$query = $_GET['query'];
 	// $query = 'bacon';
 	$jresponse = file_get_contents('http://api.yummly.com/v1/api/recipes?_app_id=6e415947&_app_key=5e4133f9b50bb1bf39382a83d84b8d9e&q=&allowedIngredient[]='.$query);
 	$recipe_list = json_decode($jresponse);
-
 	$id_1 = $recipe_list->matches[0]->id;
 	$id_2 = $recipe_list->matches[1]->id;
 	$id_3 = $recipe_list->matches[2]->id;
 	$id_4 = $recipe_list->matches[3]->id;
 	$id_5 = $recipe_list->matches[4]->id;
-
 	$url_1 = file_get_contents('http://api.yummly.com/v1/api/recipe/'.$id_1.'?_app_id=6e415947&_app_key=5e4133f9b50bb1bf39382a83d84b8d9e');
 	$url_1a = json_decode($url_1);
 	$url_2 = file_get_contents('http://api.yummly.com/v1/api/recipe/'.$id_2.'?_app_id=6e415947&_app_key=5e4133f9b50bb1bf39382a83d84b8d9e');
@@ -118,30 +112,25 @@ $app->get('/getRecipes', function($query)
 	$url_4a = json_decode($url_4);
 	$url_5 = file_get_contents('http://api.yummly.com/v1/api/recipe/'.$id_5.'?_app_id=6e415947&_app_key=5e4133f9b50bb1bf39382a83d84b8d9e');
 	$url_5a = json_decode($url_5);
-
 	$url_a = preg_replace('/(\/+)/','/',$url_1a->source->sourceRecipeUrl);
 	$url_b = preg_replace('/(\/+)/','/',$url_2a->source->sourceRecipeUrl);
 	$url_c = preg_replace('/(\/+)/','/',$url_3a->source->sourceRecipeUrl);
 	$url_d = preg_replace('/(\/+)/','/',$url_4a->source->sourceRecipeUrl);
 	$url_e = preg_replace('/(\/+)/','/',$url_5a->source->sourceRecipeUrl);
-
 	$recipe_array  = array();
 	$recipe_array[$recipe_list->matches[0]->recipeName] = $url_a;
 	$recipe_array[$recipe_list->matches[1]->recipeName] = $url_b;
 	$recipe_array[$recipe_list->matches[2]->recipeName] = $url_c;
 	$recipe_array[$recipe_list->matches[3]->recipeName] = $url_d;
 	$recipe_array[$recipe_list->matches[4]->recipeName] = $url_e;
-
 	$recipe_array = json_encode($recipe_array);
-
 	echo $recipe_array;
 });
 $app -> POST('/login', function() use ($database){
-	session_start();
 	$email = $_POST['email'];
     $password = $_POST['password'];
 	$query = "SELECT uid FROM User WHERE email = '$email' AND password = '$password' LIMIT 1" or die ("Error querying user database");
-	$run = $databse -> query($query);
+	$run = $database -> query($query);
 	$result = $run->fetch_assoc();
 	if ($result === NULL){
 		$response = array("success"=>false);
@@ -152,12 +141,12 @@ $app -> POST('/login', function() use ($database){
 	}
 	echo json_encode($response);
 });
-$app -> POST('/register', function() use ($database)){
+$app -> POST('/register', function() use ($database){
 	$fname = $_POST['firstname'];
 	$lname = $_POST['lastname'];
 	$email = $_POST['username'];
 	$password = $_POST['password'];
-	$query = "INSERT INTO User values ('$fname', '$lname', '$email', '$password', 0, 0)";
+	$query = "INSERT INTO User(fname, lname, email, password) values ('$fname', '$lname', '$email', '$password')";
 	$database->query($query);
 	$query = "SELECT uid from User where email = '$email'";
 	$run = $database->query($query);
@@ -166,8 +155,6 @@ $app -> POST('/register', function() use ($database)){
 	$response = array("success" => true);
 	echo json_encode($response);
 });
-
-
 $app->run();
 ?>
 
