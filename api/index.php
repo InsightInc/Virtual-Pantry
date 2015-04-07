5125d12cc2c407eaf11732c84a06e989a012f67d
vagrant <vagrant@scotchbox>
2015-04-07 23:48:42 +0000

    test

--- api/index.php
+++ api/index.php
@@ -94,6 +94,9 @@ $app->get('/getPantryList', function()
 	echo $response;
 });
 
+// function make_links_clickable($text){
+//     return preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $text);
+
 $app->get('/getRecipes', function()
 // $app->get('/', function()
 {
