2415a4444e0f40dc5d5381eef700953156f1cef9
redsonsmu <Redson@smu.edu>
2015-05-01 13:27:06 -0500

    Child rows?

--- js/home.js
+++ js/home.js
@@ -1,8 +1,27 @@
 String.prototype.capitalize = function(){
-            return this.toLowerCase().replace( /\b\w/g, function (m) {
-                return m.toUpperCase();
-            });
-        };
+    return this.toLowerCase().replace( /\b\w/g, function (m) {
+        return m.toUpperCase();
+    });
+};
+
+/* Formatting function for row details - modify as you need */
+function format ( d ) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+        '<tr>'+
+            '<td>Full name:</td>'+
+            '<td>'+d.name+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extension number:</td>'+
+            '<td>'+d.extn+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extra info:</td>'+
+            '<td>And any further details here (images etc)...</td>'+
+        '</tr>'+
+    '</table>';
+}        
 
 var pantryTable, recipeTable;
 $(document).ready(function(){
@@ -26,10 +45,38 @@ $(document).ready(function(){
 
     //pantryTable
     pantryTable = $("#pantryList").DataTable({
+        "columns": [
+            {
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
+            {"data": "Product Name"},
+            {"data": "Remove"}
+        ],
         "scrollY":          "300px",
         "scrollCollapse":   false,
         "paging":           false
     });
+
+    // Add event listener for opening and closing details
+    $('#pantryList tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+ 
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    } );
+
 
     //Seach for new Product with name
     $("#newProductName").keyup(function(event) {
