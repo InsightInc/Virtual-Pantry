68784cbcf4040d994743ba1b5df828941f57794d
ndantonelli <nantonelli@smu.edu>
2015-05-08 18:37:29 -0500

    added app functionality

--- api/index.php
+++ api/index.php
@@ -6,6 +6,73 @@ $app = new \Slim\Slim();
  $database = new mysqli("localhost", "root", "root", "VirtualPantryDB");
  if ($database->connect_errno)
      die("Connection failed: " . $database->connect_error);
+$app->get('/addProductUPC', function(){
+	global $database;
+	$id = $_GET['uid'];
+	$upc = $_GET['upc'];
+	#Connect to foodessentials api ------------------------------------------------
+	#Get session id
+	$sjson = file_get_contents('http://api.foodessentials.com/createsession?uid=ert&devid=ert&appid=ert&f=json&v=2.00&api_key=x4c59ktead886t2urzcdju54');
+	$sobj = json_decode($sjson);
+	$sid = $sobj->session_id;
+	#Create Profile, so information is outputted correctly
+	$profjson = file_get_contents("./profile.json");
+	$profjson = json_decode($profjson);
+	$profjson[0]['session_id'] = $sid;
+	
+	#Set Profile
+	$options = array(
+		'http' => array(
+			'method'  => 'POST',
+			'content' => json_encode($profjson),
+			'header'=>  "Content-Type: application/json\r\n" .
+			"Accept: application/json\r\n"
+			)
+		);
+	$context  = stream_context_create( $options );
+	$result = file_get_contents('http://api.foodessentials.com/setprofile?api_key=x4c59ktead886t2urzcdju54', false, $context );
+	$response = json_decode( $result );
+	//Get info for product using the upc code
+	$product = file_get_contents('http://api.foodessentials.com/productscore?u='.$upc.'&sid='.$sid.'&f=json&api_key=x4c59ktead886t2urzcdju54');
+	$product = json_decode($product);
+	
+	//Put product info into json array for the response
+	$jsonProduct = array();
+	$jsonProduct['fat'] = $product->product->nutrients[6]->nutrient_value;
+	$jsonProduct['chol'] = $product->product->nutrients[1]->nutrient_value;
+	$jsonProduct['sodium'] = $product->product->nutrients[3]->nutrient_value;
+	$jsonProduct['carb'] = $product->product->nutrients[5]->nutrient_value;
+	$jsonProduct['protien'] = $product->product->nutrients[2]->nutrient_value;
+	$jsonProduct['barcode'] = $upc;
+	$jsonProduct['cal'] = $product->product->nutrients[0]->nutrient_value;
+	$jsonProduct['name'] = $product->product->product_name;
+	$jsonProduct['manuf'] = $product->product->manufacturer;
+	
+	
+	//Put product info into varibles for the database insertions 
+	$fat = $jsonProduct['fat'];
+	$chol = $jsonProduct['chol'];
+	$sodium = $jsonProduct['sodium'];
+	$carb = $jsonProduct['carb'];
+	$protien = $jsonProduct['protien'];
+	$barcode = $jsonProduct['barcode'];
+	$cal = $jsonProduct['cal'];
+	$pname = $jsonProduct['name'];
+	$manuf = $jsonProduct['manuf'];
+	
+	//Insert product into PantryList table for user
+	$response = $database->query("INSERT INTO PantryList (uid, pname, barcode) VALUES ('$id', '$pname', '$upc')");
+	
+	
+	//Insert product into Ingredient table
+	$database->query("INSERT INTO Ingredient VALUES('$upc', '$fat', '$chol', '$sodium', '$carb', '$protien', NULL, NULL, '$cal', NULL, '$pname', '$manuf')");
+	
+	$response = $database->query("SELECT '$pname' FROM PantryList WHERE uid = $id");
+	$response = $response->fetch_all();
+	$response = json_encode($response);
+	echo $response;
+
+});
 #get product by searching by name
 $app->get('/addProduct', function() {
 	global $database;
@@ -163,6 +230,8 @@ $app->get('/removeProduct', function()
    	{
 		 $id =  $_SESSION['uid'];
    	}
+   	else
+		$id = $_GET['uid'];
 
    	//Delete item from User's Pantry
 	$response = $database->query("DELETE FROM PantryList WHERE pname = '$name' AND uid = '$id'");
@@ -195,6 +264,8 @@ $app->get('/getPantryList', function()
    	{
 		 $id =  $_SESSION['uid'];
    	}
+   	else
+		$id = $_GET['uid'];
 
    	//Get all items in User's pantry and return it as JSON 
 	$response = $database->query("SELECT pname, barcode FROM PantryList WHERE uid = $id");
@@ -208,7 +279,10 @@ $app->get('/getRecipes', function()
 {
 
 	global $database;
-	$id = $_SESSION['uid'];
+	if(isset($_SESSION['uid']))
+		$id = $_SESSION['uid'];
+	else
+		$id = $_GET['uid'];
 
 	// $id = 2;
 
@@ -259,6 +333,15 @@ $app->get('/getRecipes', function()
 	$recipe_array = json_encode($array_recipes);
 	echo $recipe_array;
 });
+$app -> POST('/loginApp', function() use ($database){
+
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$query = "SELECT uid, fname, lname FROM User WHERE email = '$email' AND password = '$password' LIMIT 1" or die ("Error querying user database");
+	$run = $database -> query($query);
+	$result = $run->fetch_assoc();
+	echo json_encode($result);
+});
 $app -> POST('/login', function() use ($database){
 
 	$email = $_POST['email'];
@@ -278,6 +361,26 @@ $app -> POST('/login', function() use ($database){
 $app -> POST('/logout', function() use ($database){
 	session_destroy();
 	$result = array("success" => true);
+	echo json_encode($result);
+});
+$app -> POST('/registerApp', function() use ($database){
+	$fname = $_POST['firstname'];
+	$lname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	$emailStatus = $database->query("SELECT * FROM User WHERE email = '$email'");
+	$emailCount = $emailStatus->num_rows;
+	if($emailCount == 0){
+		$query = "INSERT INTO User(fname, lname, email, password) values ('$fname', '$lname', '$email', '$password')";
+		$database->query($query);
+		$query = "SELECT uid from User where email = '$email'";
+		$run = $database->query($query);
+		$result = $run -> fetch_assoc();
+	}
+	
+	else
+		$result = array("uid" => 0);
 	echo json_encode($result);
 });
 $app -> POST('/register', function() use ($database){
@@ -383,7 +486,10 @@ $app->POST('/changeDiet', function(){
 $app->get('/getDiet', function()
 {
 	global $database;
-	$id = $_SESSION['uid'];
+	if(isset($_SESSION['uid']))
+		$id=$_SESSION['uid'];
+	else
+		$id = $_GET['uid'];
 
 	$result = $database->query("SELECT name, id FROM DietaryRestrictions INNER JOIN DietaryKey ON DietaryRestrictions.restricts = DietaryKey.id WHERE uid = '$id'");
 	if($result)
